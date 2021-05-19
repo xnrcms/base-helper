d3b9b9743d6e4a4dd7258564360bc4ad1a932597
小能人 <562909771@qq.com>
2021-05-20 01:24:41 +0800

    新增JWT工具包

--- src/Hash.php
+++ src/Hash.php
@@ -1,17 +1,12 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2015 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: yunwuxin <448901948@qq.com>
-// +----------------------------------------------------------------------
 
 namespace Xnrcms\BaseHelper;
 
-
+/**
+ * 密码加密助手
+ * Class Hash
+ * @package Xnrcms\BaseHelper
+ */
 class Hash
 {
     protected static $handle = [];
